3932589f3dc67a1107eb8362c92d7da963f43aa8
Nouran <only1nour@ymail.com>
2018-08-30 19:27:52 +0200

    Displaying the searched books in search page according to query

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -7,32 +7,38 @@ class SearchBooks extends Component {
 
     state = {
         query: '',
-        books: []
+        books: [],
     }
 
     updateQuery = (query) => {
+        // Fetch searched books from API and update state to re-render
         this.setState({
             query: query
         })
+
+        if(query) {
+            BooksAPI.search(query).then(books => {
+                this.setState({
+                    books: books
+                })
+            })
+        }
+        else {
+            this.setState({
+                books: []
+            })
+        }
     }
 
     render() {
 
-        const { query } = this.state
+        const { query, books } = this.state
 
         return (
             <div className="search-books">
                 <div className="search-books-bar">
                 <Link className="close-search" to="/">Close</Link>
                 <div className="search-books-input-wrapper">
-                    {/*
-                    NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                    You can find these search terms here:
-                    https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                    However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                    you don't find a specific author or title. Every search is limited by search terms.
-                    */}
                     <input 
                     type="text" 
                     placeholder="Search by title or author" 
@@ -45,7 +51,32 @@ class SearchBooks extends Component {
                 </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid"></ol>
+                    <ol className="books-grid">
+                        {/* If the query is empty or can't fetch the books from the api, clear the following section */}
+                        {query !== '' && !books.error && books.map(book => (
+                          <li key={book.id}>
+                            <div className="book">
+                              <div className="book-top">
+                              {/* Check for the book's thumbnail as not to break the program */}
+                              {book.hasOwnProperty('imageLinks') && 
+                              (<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>)}
+                                
+                                <div className="book-shelf-changer">
+                                  <select>
+                                    <option value="move" disabled>Move to...</option>
+                                    <option value="currentlyReading">Currently Reading</option>
+                                    <option value="wantToRead">Want to Read</option>
+                                    <option value="read">Read</option>
+                                    <option value="none">None</option>
+                                  </select>
+                                </div>
+                              </div>
+                              <div className="book-title">{book.title}</div>
+                              <div className="book-authors">{book.authors}</div>
+                            </div>
+                          </li>
+                        ))}
+                    </ol>
                 </div>
           </div>
         )
